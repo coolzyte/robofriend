b977496f8e9d670473bdc1b7a3c2a806a8e0f591
coolzyte <coolzyte@gmail.com>
2018-11-16 15:35:13 +0100

    worked on the cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,7 +4,9 @@ import "./Card.scss";
 const card = ({ id, name, email }) => {
   return (
     <div className="Card">
-      <img src={`https://robohash.org/${id}?200x200`} alt="" />
+      <div className="Card__Image">
+        <img src={`https://robohash.org/${id}?200x200`} alt="" />
+      </div>
       <div className="Card__Content">
         <h2 className="Card__Title">{name}</h2>
         <p>{email}</p>
