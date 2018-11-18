5651078fea6c6a51a665e8c6d880e52f2c6f4a2a
coolzyte <coolzyte@gmail.com>
2018-11-18 20:37:19 +0100

    Updates

---
+++ precache-manifest.d0cc4b6148aac13b1af6a89f40668aa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8d08ed278feca8adcb49",
+    "url": "/robofriend/static/js/runtime~main.8d08ed27.js"
+  },
+  {
+    "revision": "52ba6b77088867de01be",
+    "url": "/robofriend/static/js/main.52ba6b77.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/robofriend/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "52ba6b77088867de01be",
+    "url": "/robofriend/static/css/main.46521e00.chunk.css"
+  },
+  {
+    "revision": "7fd759349ac3275df0cd932040c7d43a",
+    "url": "/robofriend/index.html"
+  }
+];
