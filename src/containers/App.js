95dd0a3f58da48fbc1fe2fbedf840a8de79e8016
coolzyte <coolzyte@gmail.com>
2018-11-18 20:22:31 +0100

    cleaned up app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -22,29 +22,26 @@ class App extends Component {
     this.setState({ searchfield: event.target.value });
   };
   render() {
-    const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    if (this.state.robots.length === 0) {
-      return <h1>Loading</h1>;
-    } else {
-      return (
-        <div className="container App">
-          <div className="header">
-            <h1>RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange} />
-          </div>
-          <div className="main">
-            <Scroll>
-              <CardList robots={filteredRobots} />
-            </Scroll>
-          </div>
-          <div className="footer">&copy; Created by Coolzyte</div>
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="container App">
+        <div className="header">
+          <h1>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
         </div>
-      );
-    }
+        <div className="main">
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+        <div className="footer">&copy; Created by Coolzyte</div>
+      </div>
+    );
   }
 }
 
