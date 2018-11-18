7e8f56f4d9f6b7c8ce1a4c7fc1984079e96a1616
coolzyte <coolzyte@gmail.com>
2018-11-18 05:03:04 +0100

    fetching robots details from a remote host

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 import "./App.scss";
 import CardList from "../components/CardList/CardList";
 import SearchBox from "../components/SearchBox/SearchBox";
-import { robots } from "../robots";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ""
     };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   onSearchChange = event => {
@@ -22,13 +26,17 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="App Wrapper">
-        <h1>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="App Wrapper">
+          <h1>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
